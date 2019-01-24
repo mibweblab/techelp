8de5112e4845fe830f072c38b1a5e41d81eb3613
mrrCarter <carther@mit.edu>
2019-01-23 20:44:20 -0800

    added the route handler that will make sure that when someone is logged in we can see them logged in and stays logged in

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,4 +10,12 @@ module.exports = app => {
   );
   //callback route handler
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  //whenever someone makes a get request to our app, we will have a route that returns whoever is currently logged in
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+    res.send('Or we can have something like: Welcome ', req.user.displayName);
+    // console.log(req);
+    console.log('your request came from:', req.user);
+  });
 };
